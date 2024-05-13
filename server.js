3b78be7b1717d2385583e3d1bb8b6dcbea12b7b8
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2024-05-13 20:22:17 +0530

    connect node with mongodb

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' }); //this command will do read our variable from the file and save them in nodejs enviroment variable
 
-const app = require('./app');
+/**MongoDB */
+/** connection string for mongoose i.e DB */
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+/** mongoose.connect() is return a promise so we have to hanndle the promise using then() which have access to connection object which is con,con is the resolve value of promise*/
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection succesfull :💽💾💾💾'));
 
 /**------StartServer------------------------------*/
 const port = process.env.PORT || 3000;
