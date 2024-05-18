2fdbb36bf61012179ab6bef7d41b120713775995
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2024-05-19 01:32:33 +0530

    uncaught exception

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+/**Catching uncaught Exception(top of code ) */
+
+process.on('uncaughtException', err => {
+  console.log('Uncaught Exception shutting down ....');
+  console.log(err.name, err.message);
+  //we really need to crach our application,because after there was an uncaught expection the entire node process is so called un-clean state
+  process.exit(1);
+});
 
 dotenv.config({ path: './config.env' }); //this command will do read our variable from the file and save them in nodejs enviroment variable
 
@@ -24,6 +32,28 @@ mongoose
 /**------StartServer------------------------------*/
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App start at port : ${port} 🚀🚀🚀`);
 });
+
+/** Globally handle Unhandle rejected promises */
+/**
+ Each time that there is unhandle rejection somewhere in our application,the process-obj  will emit an object called unhandle rejection,so that we can subscribe to that event
+ */
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message); //MongoError bad auth : authentication failed
+  /**if you have Promblem with DB so just exit the programm/shutDown the application */
+  /**
+   code zero stands for success
+   one stands for uncaught exceptions
+   */
+  console.log('Unhandle Rejection 😥😥😥,Shuting down application');
+  /**
+    server.close() we giving server,time to finish all request that are still pending or begin handle all the time,aft that the server basically closed
+   */
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// console.log(x); //x is undefined
