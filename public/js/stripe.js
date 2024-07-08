ff0cfc89d47748b6708ce45446acec5fe4bfce8c
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2024-07-09 01:04:15 +0530

    fix stripe

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-
-const stripe = Stripe(
-  'pk_test_51PZqQ5IxMdURXTfy804fwNYPiXxWqX3f0D8zenRYfUpIzQLm42AES8oYA9KStVh4k1dS7w967AxlRYUNnJQLCx5w00gg5TBMP1'
-);
+// import Stripe from 'stripe';
 
 export const bookTour = async tourId => {
+  const stripe = Stripe(
+    'pk_test_51PZqQ5IxMdURXTfy804fwNYPiXxWqX3f0D8zenRYfUpIzQLm42AES8oYA9KStVh4k1dS7w967AxlRYUNnJQLCx5w00gg5TBMP1'
+  );
   try {
     // 1) Get checkout session from API
     const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
